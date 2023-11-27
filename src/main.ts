e017815125065a841d8882c7fe5ab6a012b48c14
bsubba <bikaash_subba@yahoo.co.uk>
2023-11-27 16:05:08 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api");
+  app.enableCors();
   await app.listen(6000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
